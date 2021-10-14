b59b1a1e42c6429f3f9c406eff91bcaf4ef342fd
Quan Le <quanle@Quans-iMac.local>
2021-10-13 18:13:54 -0700

    do assignment 2 , version 2

--- calculator.py
+++ calculator.py
@@ -19,7 +19,7 @@ def calculator(number1, number2, operator):
 
 	result = 0
 	
-	if type(number1) == int or float and type(number2) == int or float:
+	if type(number1) == int or float and type(number2) == int or float and operator in ('+','-','*','/','//','**'):
 		num1 = float(number1)
 		num2 = float(number2)
 		if operator == '+':	
@@ -33,11 +33,11 @@ def calculator(number1, number2, operator):
 		elif operator == '//':	
 			result= num1 // num2	 	
 		elif operator == '**':	
-			result= num1 ** num2	 		
+			result= num1 ** num2
+		return result	 		
 	else:
-		print('Invalid value!!!')
+		print('Invalid value/operator!!!')
 		return False
-	return result
 
 def parse_input():
 	"""
@@ -58,14 +58,19 @@ def parse_input():
 
 	get input from user
 	split the input by a delimter that is space
+	check user'input
 	pass these values which just split to the calculator function with 3 parameters
 	"""
 
-	prompt = eval('input("Enter equation: ")')
+	prompt = eval("input('Enter equation: ')")
 	text = prompt.split(' ')
-	num1 = text[0]
-	num2 = text[2]
-	operator = text[1]
+	if len(text) == 3:
+		num1 = text[0]
+		num2 = text[2]
+		operator = text[1]
+	else:
+		print('Not valid!')
+		exit() 	
 	return calculator(num1,num2,operator)
 
 print(parse_input())
