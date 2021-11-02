77d4bc75bd6ff10d68816ff1aae8d57dfbc5e261
Quan Le <quanle@Quans-iMac.local>
2021-11-01 19:04:28 -0700

    version2

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,5 +1,8 @@
 from myapp import myapp_obj
+from flask import render_template
 
 @myapp_obj.route("/")
 def homepage():
-	return "Hello"
+	title_cities = 'Top Cities'
+	greeting = "Quan"
+	return render_template("base.html", title_pass = title_cities, XX = greeting)
