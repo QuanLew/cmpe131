f7ecde65d977da23b5bdf5192d5364eda04847ce
Quan Le <quanle@Quans-iMac.local>
2021-11-03 01:09:02 -0700

    version5

--- myapp/models.py
+++ myapp/models.py
@@ -5,8 +5,15 @@ class Cities(db.Model):
 	cityname = db.Column(db.String(64), unique = True, index = True)
 	cityrank = db.Column(db.Integer, unique = True)
 
+	def __init__(self,cityname,cityrank):
+		self.cityname = cityname
+		self.cityrank = cityrank
+
 	def set_rank(self,cityrank):
 		self.cityrank = cityrank
+
+	def set_name(self,cityname):
+		self.cityname = cityname
 
 	def __repr__(self):
                 return f'{self.cityname}'
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,5 +1,5 @@
 from myapp import myapp_obj
-from flask import render_template
+from flask import flash,render_template, redirect
 from myapp import db
 from myapp.models import Cities
 from myapp.forms import TopCities
@@ -10,4 +10,22 @@ def homepage():
 	greeting = "Quan"
 	form_city = TopCities()
 	model_city = Cities.query.all()
+
+	if form_city.validate_on_submit():
+		#name = Cities.query.filter_by(cityname = form_city.city_name.data).first()
+		name = form_city.city_name.data
+		rank = form_city.city_rank.data
+
+		if name is None:
+			flash('Invalid input!!!')
+		else:
+			flash(f'{form_city.city_name.data} is added')
+			db.create_all()		
+			city = Cities(name,rank)
+			#city.set_name(name)
+			#city.set_rank(rank)
+			db.session.add(city)
+			db.session.commit()	
+		return redirect('/')
+	
 	return render_template("home.html", title_pass = title_cities, XX = greeting, top_cities = form_city, name_cities = model_city)
