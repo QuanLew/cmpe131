11a6cdd0bd5ab9da0084b4b611c5b9f74e909a29
QuanLew <90435027+QuanLew@users.noreply.github.com>
2021-11-18 13:18:17 -0800

    Delete myapp directory

--- myapp/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, InputRequired
-
-class TopCities(FlaskForm):
-	city_name = StringField("City Name", validators = [DataRequired()])
-	city_rank = IntegerField("City Rank", validators = [InputRequired()])
-	is_visited = BooleanField("Visited")
-	submit_form = SubmitField("Submit")
